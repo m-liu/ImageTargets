1cb938242cc4343e2643c61078084fea838d0d98
Ming <Ming@Ming-WIN7>
2012-01-14 22:58:44 -0500

    Android.mk test

--- jni/Android.mk
+++ jni/Android.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := $(call my-dir)
 # and setting the include path for library-specific header files
 
 include $(CLEAR_VARS)
-QCAR_SDK_ROOT = ../../../qcar_sdk
+QCAR_SDK_ROOT = ../../../AllTools/qcar-android-1-0-6
 LOCAL_MODULE := QCAR-prebuilt
 LOCAL_SRC_FILES = $(QCAR_SDK_ROOT)/build/lib/$(TARGET_ARCH_ABI)/libQCAR.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(QCAR_SDK_ROOT)/build/include
